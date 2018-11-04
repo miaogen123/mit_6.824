037cecc2586366053fd0732bbd25e245cc7ec88e
miaogen <miaogen156@outlook.com>
2018-11-04 20:07:44 +0800

    lab1-task1

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,13 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTask int, // which reduce task this is
@@ -12,7 +20,7 @@ func doReduce(
 	// files for the task, sort the intermediate key/value pairs by key,
 	// call the user-defined reduce function (reduceF) for each key, and
 	// write reduceF's output to disk.
-	//
+	// 读中间文件
 	// You'll need to read one intermediate file from each map task;
 	// reduceName(jobName, m, reduceTask) yields the file
 	// name from map task m.
@@ -44,4 +52,44 @@ func doReduce(
 	//
 	// Your code here (Part I).
 	//
+	//fmt.Println(jobName, reduceTask, outFile, nMap)
+	//duration := time.Duration(2) * time.Second
+	//time.Sleep(duration)
+	//NOTE::这里有个问题，sequential的时候，nMap传过来为1，reduceTask 却为0
+	//在创建文件*_map.go 时，nmap为0， reduce task却为1， 导致输入和输出的文件不一样
+	//FIX：这里是我的理解的问题，题目中nMap, 我理解 成的被写的文件，实际上应该是，有多少要写入的文件
+	for nmap := 0; nmap < nMap; nmap++ {
+		readfile := reduceName(jobName, nmap, reduceTask)
+		readfileHandler, err := os.OpenFile(readfile, os.O_RDONLY, 0664)
+		defer readfileHandler.Close()
+		content, _ := ioutil.ReadAll(readfileHandler)
+		if err != nil {
+			panic(err)
+		}
+		//直接写入文件
+		//从文件中读取，花了一点功夫在GO的语法上面
+		var kvList []KeyValue
+
+		json.Unmarshal(content, &kvList)
+		sort.Slice(kvList, func(a, b int) bool {
+			i, err := strconv.ParseInt(kvList[a].Key, 10, 32)
+			j, err := strconv.ParseInt(kvList[b].Key, 10, 32)
+			if err != nil {
+				panic(err)
+			}
+			return i < j
+		})
+		writeTofileHandler, err := os.OpenFile(outFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+		defer writeTofileHandler.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		encoderHandler := json.NewEncoder(writeTofileHandler)
+		for _, val := range kvList {
+			var valList []string
+			valList = append(valList, val.Value)
+			encoderHandler.Encode(KeyValue{val.Key, reduceF(val.Key, valList)})
+		}
+	}
 }
