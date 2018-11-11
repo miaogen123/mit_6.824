371f552d4fa66829ae9010c25314dc01ef92cff8
miaogen <miaogen156@outlook.com>
2018-11-11 17:17:02 +0800

    lab1-task3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +34,65 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+	var workerSet []string
+	//tmpFile, errFile := os.OpenFile("tmp", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+	//if errFile != nil {
+	//	panic("文件打开出错")
+	//}
+	//tmpFile.Write([]byte(jobName + " " + strconv.Itoa(ntasks) + " " + strconv.Itoa(n_other) + " " + string(phase) + "\n"))
+	workerAddr := ""
+	//true为正在工作，false表示已经完成
+	flag := true
+	workerState := make(map[string]bool)
+	//读取所有的worker
+	for flag {
+		select {
+		case workerAddr, _ = <-registerChan:
+			workerSet = append(workerSet, workerAddr)
+			workerState[workerAddr] = false
+		case <-time.After(time.Second):
+			fmt.Println("After one second!")
+			flag = false
+			break
+		}
+	}
+	runningTask := 0
+	var wg sync.WaitGroup
+	wg.Add(len(workerSet))
+	//tmpFile.Write([]byte(strconv.Itoa(len(workerSet)) + " \n"))
+	for i := 0; i < ntasks; i++ {
+		//FIXME：：这里有需要优化的地方：就是这里等待goroutine结束的时候，使用的是waitGroup，没有用一种等待其中一个OK的方式，(原因：还不会)
+		for key, val := range workerState {
+			if val == false {
+				workerAddr = key
+			}
+		}
+		//tmpFile.Write([]byte(workerAddr + " \n"))
+		doTaskArgsToPass := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+		go func() {
+			call(workerAddr, "Worker.DoTask", doTaskArgsToPass, nil)
+			wg.Done()
+		}()
+		runningTask++
+		workerState[workerAddr] = true
+		if runningTask == len(workerSet) {
+			wg.Wait()
+			runningTask = 0
+			for key := range workerState {
+				workerState[key] = false
+			}
+			wg.Add(len(workerSet))
+		}
+		//监听新来的请求
+		//FIXME::这里每次只会监听一个新加入的请求
+		select {
+		case workerAddr, _ = <-registerChan:
+			workerSet = append(workerSet, workerAddr)
+			workerState[workerAddr] = false
+			wg.Add(1)
+		case <-time.After(time.Millisecond * 200):
+		}
+	}
+	//tmpFile.Write([]byte(string(phase) + "finished \n"))
 	fmt.Printf("Schedule: %v done\n", phase)
 }
